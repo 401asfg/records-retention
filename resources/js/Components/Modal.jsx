01b8fc63a03d5598f4648bd4ad8a529bc8c64178
401asfg <mb.allan@ymail.com>
2024-10-15 11:51:49 -0700

    Modifed modal to scale properly for different resolutions and improved the layout of the modal

--- resources/js/Components/Modal.jsx
+++ resources/js/Components/Modal.jsx
@@ -7,7 +7,7 @@ const Modal = (props) => {
     return ReactDom.createPortal(
         <>
             <div onClick={props.onClose} className="position-fixed top-0 bottom-0 z-9" style={{background: "rgba(0, 0, 0, .7)", left: 0, right: 0}} />
-            <div className="position-fixed top-50 translate-middle bg-light p-3 z-9" style={{left: "50%"}}>
+            <div className="col-md-6 col-10 position-fixed top-50 translate-middle bg-light p-3 z-9" style={{left: "50%"}}>
                 <div className='mb-4'>
                     {props.children}
                 </div>
