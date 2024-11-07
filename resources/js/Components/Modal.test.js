4d4b51b70600fbb0105d6b898a463a5d10df957f
401asfg <mb.allan@ymail.com>
2024-11-07 10:23:38 -0800

    Implemented modal test suite

--- resources/js/Components/Modal.test.js
+++ resources/js/Components/Modal.test.js
@@ -4,13 +4,25 @@ import { render, screen, fireEvent, waitFor, act } from '@testing-library/react'
 import React from 'react';
 
 describe(Modal, () => {
+    const onClose = jest.fn();
+
     function renderModal(isOpen) {
         render(
-            <Modal isOpen={isOpen} onClose={() => {}}>
+            <Modal isOpen={isOpen} onClose={onClose}>
                 <div data-testid="modal_content">Modal Content</div>
             </Modal>
         );
     }
+
+    beforeAll(() => {
+        const portal = document.createElement('div');
+        portal.setAttribute('id', 'portal');
+        document.body.appendChild(portal);
+    })
+
+    beforeEach(() => {
+        onClose.mockClear();
+    })
 
     describe("Initialization", () => {
         it("should not render if isOpen is false", () => {
@@ -28,5 +40,17 @@ describe(Modal, () => {
         });
     })
 
-    // TODO: write rest of tests
+    describe("Closing", () => {
+        it("should call onClose when clicking the background", () => {
+            renderModal(true);
+            fireEvent.click(screen.getByTestId("modal_background"));
+            expect(onClose).toHaveBeenCalledTimes(1);
+        });
+
+        it("should call onClose when clicking the close button", () => {
+            renderModal(true);
+            fireEvent.click(screen.getByTestId("close_modal"));
+            expect(onClose).toHaveBeenCalledTimes(1);
+        });
+    })
 });
